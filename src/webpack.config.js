bc91b6c1b6ae0d0dcd2220ef67cbb2a38e391af3
Manoj Kumar <113422475+mkskymoon@users.noreply.github.com>
2024-08-07 15:17:05 +0530

    Update webpack.config.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   target: 'node', // important for handling node-specific modules
   output: {
     filename: 'bundle.js',
